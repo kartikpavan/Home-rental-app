f163bed41da981523dee6593a923a75746af33bb
Pavan <kartik.varanasi@yahoo.com>
2024-01-18 18:41:16 +0530

    category filter implemented

--- client/src/components/Listings.tsx
+++ client/src/components/Listings.tsx
@@ -46,30 +46,47 @@ const Listings = () => {
                   return (
                      <div
                         key={idx}
-                        className="w-full max-w-32 h-24 rounded-md shadow-md border border-gray-300 p-5 grid place-items-center gap-3">
-                        <p>{label}</p>
+                        onClick={() => setSelectedCategory(label)}
+                        className={`w-full max-w-32 h-24 rounded-md shadow-md border p-5 grid place-items-center gap-3 ${
+                           label === selectedCategory
+                              ? "border-primary border-1 bg-primary/10"
+                              : "border-gray-300"
+                        }`}>
+                        <p className="text-primary-content">{label}</p>
                         <Icon />
                      </div>
                   );
                })}
             </section>
             {isLoading ? (
-               <p className="text-4xl font-bold">Loading...</p>
+               <div className="flex justify-center w-full items-center">
+                  <span className="loading loading-spinner loading-lg"></span>
+               </div>
             ) : (
                <section className="px-3 md:container mx-auto pt-10 pb-20 flex gap-x-10 gap-y-20 flex-wrap justify-center">
-                  {listings.map((listing) => {
-                     return (
-                        <ListingCard
-                           key={listing._id}
-                           listingId={listing._id}
-                           title={listing.title}
-                           categories={listing.categories}
-                           images={listing.images}
-                           price={listing.price}
-                           accomodation={listing.accomodation}
-                        />
-                     );
-                  })}
+                  {listings.length > 0 ? (
+                     listings.map((listing) => {
+                        return (
+                           <ListingCard
+                              key={listing._id}
+                              listingId={listing._id}
+                              title={listing.title}
+                              categories={listing.categories}
+                              images={listing.images}
+                              price={listing.price}
+                              accomodation={listing.accomodation}
+                           />
+                        );
+                     })
+                  ) : (
+                     <p className="text-center font-bold text-xl ">
+                        No listings found for{" "}
+                        <span className="underline italic text-primary-content/80">
+                           "{selectedCategory}"
+                        </span>{" "}
+                        category
+                     </p>
+                  )}
                </section>
             )}
          </div>
