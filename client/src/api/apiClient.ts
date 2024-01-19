a743a47553652037b0d66c7663bae9851387db67
Pavan <kartik.varanasi@yahoo.com>
2024-01-20 01:18:51 +0530

    vercel.json added for re-routing

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,5 +1,5 @@
 export const DEV_URL = "http://localhost:5000" as const;
 
-export const PROD_URL = "production-endpoint" as const;
+export const PROD_URL = "https://dream-harbor.onrender.com" as const;
 
 export const API_URL = import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
