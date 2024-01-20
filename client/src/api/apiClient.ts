4c4a8652cdf8c535dcab6e7408a0b331035d1d69
Pavan <kartik.varanasi@yahoo.com>
2024-01-20 20:22:52 +0530

    api_client URL change

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,7 +1,7 @@
-// export const DEV_URL = "http://localhost:5000" as const;
+export const DEV_URL = "http://localhost:5000" as const;
 
-// export const PROD_URL = "https://dream-harbor.onrender.com" as const;
+export const PROD_URL = "https://dream-harbor.onrender.com" as const;
 
-// export const API_URL = import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
+export const API_URL = import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
 
-export const API_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000";
